75d8e1fc05288585d86d34c82f0bf54f5fafe758
HannePeter <hannepeter@posteo.de>
2019-07-02 08:48:23 +0200

    created basic functions for timeline (not testet)

--- src/datamodel/timeline.cpp
+++ src/datamodel/timeline.cpp
@@ -1,11 +1,31 @@
 #include "timeline.h"
 
-timeline::timeline(string identifier)
+
+
+timeline::timeline(int identifier)
+: id(identifiert)
 {
-    this->identifier = identifier;
+    for (int i = 0; i < MAXSLOTS; i++)      // set 96 empty timeslots
+        timeslots.push_back(nullprt);
 }
 
-void timeline::set_identifier(string identifier)
+
+
+void timeline::insert_task(task *new_task)
 {
-    this->identifier = identifier;
+    int hour = new_task->get_starttime().get_hour;
+    int minute = new_task->get_starttime().get_minute;
+
+    int minute_offset;
+    if (minute == 0)
+        minute_offset = 0;
+    else if (minute == 15)
+        minute_offset = 1;
+    else if (minute == 30)
+        minute_offset = 2;
+    else
+        minute_offset = 3;
+
+    int slot = (4*hour) + minute_offset;
+    timeslots.at(slot) = new_task;
 }
--- src/datamodel/timeline.h
+++ src/datamodel/timeline.h
@@ -1,44 +1,26 @@
-#ifndef DATASTRUCTURE_H_INCLUDED
-    #define DATASTRUCTURE_H_INCLUDED
+#ifndef TIMELINE_H 
+    #define TIMELINE_H TIMELINE_H
 
-#include <string>
-#include <vector>
-#include "task.h"
+    #include <vector>
+    #include "task.h"
 
+    using namespace std;
 
-using namespace std;
-
-#define MAXTASKS 96 //solange noch keine Ueberschneidungen moeglich sind(24 * 4)
-
-class timeline{
-    private:
-        string identifier;                 //07062019, Datum als Identifier? String wegen fuehrender 0
-        std::vector<int> tasks = {};       // die Nummern bzw jeweiligen Identifier der Tasks, Groesse Maxtasks?
-
-    public:
-        timeline();
-        timeline(string identifier);
-
-        //Setter
-        void set_identifier(string identifier);
-
-        //Getter
-        string get_identifier() {return identifier;}
-
-        void addTask(int task);
-        void deleteTask(int task);
+    #define MAXSLOTS 96
 
 
-};
 
+    class timeline
+    {
+        vector<task *> timeslots;   // muss im constructor mit NULL initialisiert werden
+        int id;                     // wird im constructor gesetzt, anschließend fix
 
-#endif // DATASTRUCTURE_H_INCLUDED
+        public:
+            timeline(int identifier);
 
-/*Aufkommende Fragen:
--Enums erstellen?
--dynamische Enums, Beispiel Kategorien?
--Dauer in welcher Einheit? Minuten?
--Benennung der Structs, eigene structs oder vorgegebene Bib?
--deadline: eigenes struct? Datum + Uhrzeit, Deadline Funktion mit rein?
--tools klasse fuer zeit funktionen, umwandlungen etc
-*/
+            int get_identifier() const {return identifier;}
+
+            void instert_task(task *new_task);
+    };
+
+#endif
