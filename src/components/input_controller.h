6a9307438746917cbb200fc22e587f19a1de2c59
HannePeter <hannepeter@posteo.de>
2019-07-04 22:13:12 +0200

    my english is not the yellow of the egg

--- src/components/input_controller.h
+++ src/components/input_controller.h
@@ -11,6 +11,15 @@
     #define BUT_PRINT   1
 
 
+    /**
+     * @brief   The input_controller class is the interface between the GUI and the main-prog. functions.
+     *          The GUI just has to set the data for processing and call the button_pressed-method
+     *          with button-ID (see defines on the top on this file). The input_controller is callen
+     *          the necessary functions of the main program.
+     *
+     * @maintainer:     Peter Hanne
+     * @last update:    04.07.2019
+     */
 
     class input_controller: public input_controller_intf
     {
@@ -23,7 +32,7 @@
 
         static input_controller ic_instance;                                        // Singleton: important static
 
-        string  task_title;
+        string  task_title;                                                         // Buffer for task informations
         string  task_description;
         tdate   task_date;
         ttime   task_time;
@@ -32,10 +41,36 @@
         public:
             static input_controller& get_instance();
 
+            /**
+             * @brief start() starts the input_controller component.
+             */
             void start();
+
+            /**
+             * @brief stops() starts the input_controller component.
+             */
             void stop(bool exit = false);
 
+
+            /**
+             * @brief   set_task_parameter will set all important parameters of a task.
+             *          This is needed if a Task will be created or changed.
+             *          The GUI is calling this method to set there informations.
+             *
+             * @param   All parameters are parts of the task class.
+             * @return  -1, if important parameters are missing / wrong
+             *           0, if all important parameters are ok
+             */
             int set_task_parameter(string title, string decription, int hour, int minute, int day, int month, int year);
+
+
+            /**
+             * @brief   button_pressed() is called by the GUI, when a button ins pressed.
+             *          Depending on the button, button_pressed() will call important methods
+             *          for e.g. processing tasks.
+             *
+             * @param   The button that was clicked. See defindes on the top of this file.
+             */
             void button_pressed(int button);
     };
 
